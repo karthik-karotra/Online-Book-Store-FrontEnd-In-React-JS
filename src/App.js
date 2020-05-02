9c3026f984b092dc54db2eba58d04a79bd002b5f
karthik-karotra <karthikpatel54@gmail.com>
2020-05-02 18:52:16 +0530

    ADDED:UI For Adding Book Details

--- src/App.js
+++ src/App.js
@@ -2,11 +2,15 @@ import React from 'react';
 import './App.css';
 import TopNavigationBar from "./components/TopNavigationBar";
 import BottomBar from "./components/BottomBar";
+import AdminFrontPage from "./components/AdminFrontPage";
 
 function App() {
     return (
         <div>
             <TopNavigationBar/>
+            <div className="adminfrontpage">
+                <AdminFrontPage/>
+            </div>
             <BottomBar/>
         </div>
     );
