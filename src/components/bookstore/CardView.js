6736bb15ea22c099a58b0dbf4d1b098bdd7a613c
abhishekvarmagh <abhishektheverma@gmail.com>
2020-05-09 02:42:55 +0530

    ADDED : Image To The Upper Portion Of Card

--- src/components/bookstore/CardView.js
+++ src/components/bookstore/CardView.js
@@ -1,13 +1,28 @@
 import React from 'react'
 import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardMedia from '@material-ui/core/CardMedia';
 import './CardView.css';
 
 class CardView extends React.Component {
 
+    constructor(props) {
+        super(props);
+        this.state = {
+            a: '18-the-power-of-positive-thinking'
+        }
+    }
+
     render() {
         return (
             <Card className="cards">
-
+                <CardActionArea>
+                    <CardMedia
+                        className="media"
+                        image={require(`../../images/${this.state.a}.jpg`)}
+                        title="Paella dish"
+                    />
+                </CardActionArea>
             </Card>
         )
     }
