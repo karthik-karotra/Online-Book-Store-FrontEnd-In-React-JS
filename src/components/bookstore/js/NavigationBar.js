6d01284d5d6d318acc7b7cf9c5d2e177f0550393
abhishekvarmagh <abhishektheverma@gmail.com>
2020-05-13 01:38:08 +0530

    ADDED : Search Icon In The Navigation Bar

--- src/components/bookstore/js/NavigationBar.js
+++ src/components/bookstore/js/NavigationBar.js
@@ -2,6 +2,8 @@ import React from "react";
 import {fade, makeStyles} from "@material-ui/core/styles";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
+import InputBase from "@material-ui/core/InputBase";
+import SearchIcon from "@material-ui/icons/Search";
 import Typography from "@material-ui/core/Typography";
 import MenuBookIcon from "@material-ui/icons/MenuBookSharp";
 
@@ -20,6 +22,41 @@ const useStyles = makeStyles(theme => ({
         fontSize: "120%",
         [theme.breakpoints.up("sm")]: {
             display: "block"
+        }
+    },
+    search: {
+        position: "relative",
+        borderRadius: theme.shape.borderRadius,
+        backgroundColor: fade(theme.palette.common.white, 0.15),
+        "&:hover": {
+            backgroundColor: fade(theme.palette.common.white, 0.25)
+        },
+        marginRight: theme.spacing(2),
+        marginLeft: "0",
+        width: "80%",
+        [theme.breakpoints.up("sm")]: {
+            marginLeft: theme.spacing(18),
+            width: "auto"
+        }
+    },
+    searchIcon: {
+        width: theme.spacing(6),
+        height: "100%",
+        position: "absolute",
+        pointerEvents: "none",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center"
+    },
+    inputRoot: {
+        color: "inherit"
+    },
+    inputInput: {
+        padding: theme.spacing(1, 1, 1, 10),
+        transition: theme.transitions.create("width"),
+        width: "100%",
+        [theme.breakpoints.up("md")]: {
+            width: 400
         }
     },
     sectionDesktop: {
@@ -48,6 +85,19 @@ export default function Header() {
                     <Typography className={classes.title} variant="h6" noWrap>
                         The Country Bookshop
                     </Typography>
+                    <div className={classes.search}>
+                        <div className={classes.searchIcon}>
+                            <SearchIcon/>
+                        </div>
+                        <InputBase
+                            placeholder="Search Your Book Here"
+                            classes={{
+                                root: classes.inputRoot,
+                                input: classes.inputInput
+                            }}
+                            inputProps={{"aria-label": "search"}}
+                        />
+                    </div>
                 </Toolbar>
             </AppBar>
         </div>
